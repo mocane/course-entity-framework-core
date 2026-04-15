eb9d35a665066857e14e1ba7d5cd6bb111008216
Emil Mocan <emil.mocan@protonmail.ch>
2026-04-15 17:37:18 +0300

    Add console program and EF Core Design package to it. Setup ef tools. Add initial migration.

--- EntityFrameworkCore/EntityFrameworkCore.Data/FotballLeagueDbContext.cs
+++ EntityFrameworkCore/EntityFrameworkCore.Data/FotballLeagueDbContext.cs
@@ -5,13 +5,22 @@ namespace EntityFrameworkCore.Data
 {
     public class FotballLeagueDbContext : DbContext
     {
+        private string Dbpath;
+
+        public FotballLeagueDbContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            Dbpath = System.IO.Path.Join(path, "FotballLeague.db");
+        }
+        
         public DbSet<Team> Teams { get; set; }
         
         public DbSet<Coach> Coaches { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlite("Data Source=FotballLeague.db");
+            optionsBuilder.UseSqlite($"Data Source={Dbpath}");
         }
     }
 }
